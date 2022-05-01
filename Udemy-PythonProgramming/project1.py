a11cbb3fb3048dbc3511ad9da0b7e445e28c1fbb
aminulloh <luthfi.aminulloh@gmail.com>
2022-05-01 16:38:34 +0700

    notes update

---
+++ Udemy-PythonProgramming/project1.py
@@ -0,0 +1,19 @@
+print('input [x] for exit')
+
+while True:
+    inp = input('Tell me a number: ')
+    if inp == 'x':
+        break
+    # catch any resulting ValueError during the conversion to float
+    try:
+        number = float(inp)
+    except ValueError:
+        print('I said: Tell me a NUMBER!')
+    else:
+        test = number % 2
+        if test == 0:
+            print(int(number),'is even.')
+        elif test == 1:
+            print(int(number),'is odd.')
+        else:
+            print(number,'is very strange.')
